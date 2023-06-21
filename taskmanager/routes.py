2408005a55308a6b136f68741ef7edd29383bff9
ofemiashiru <>
2023-06-21 10:02:17 +0000

    Update portion of Crud

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -40,3 +40,24 @@ def add_category():
         return redirect(url_for("categories"))
     # By default this handles the GET method
     return render_template("add_category.html")
+
+# using custom paths we can cast it as a str: or as an int: noqa
+
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
+# we also pass the custom path/variable into the function below
+def edit_category(category_id):
+
+    # we can use the get_or_404 method in order to bring back the specific
+    # id or produce a 404 message if not found
+    category = Category.query.get_or_404(category_id)
+    if request.method == "POST":
+        # if method is POST then set the called category above to the
+        # request.form.get()
+        category.category_name = request.form.get("category_name")
+        db.session.commit()
+        # remember to return your redirects
+        return redirect(url_for("categories"))
+
+    # default GET method
+    return render_template("edit_category.html", category=category)
